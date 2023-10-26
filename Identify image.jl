cf44fde35c518e8899914084d43fca578e0aef8c
Sahil Naik <nsahil992@gmail.com>
2023-10-26 18:44:35 +0530

    File added

---
+++ Identify image.jl
@@ -0,0 +1,13 @@
+using Metalhead: VGG19, preprocess, load, labels
+using Flux: onecold
+
+model = VGG19()
+class_labels = labels(model)
+
+println("Enter the name of your file: ")
+user_image = preprocess(load(readline()))
+model_prediction = model(user_image)
+top_class = onecold(model_prediction)[1]
+class_name = labels[top_class]
+
+println("I think this image contains: $(class_name)")
